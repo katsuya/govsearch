ab9ea0d2ed1da3d5170864f73c857bc5c269b68c
Yosuke Hizen <yosuke@hicustomer.jp>
2024-04-10 13:59:06 +0900

    fix(frontend): rewrite usage note

--- frontend/components/qa-help.tsx
+++ frontend/components/qa-help.tsx
@@ -10,9 +10,9 @@ import {
 const help = `
 ### Govsearchとは？
 
-Govsearchは、日本政府の公式文書を簡単に検索できる非公式のアプリケーションです。検索バーにキーワードや疑問を入力するだけで、政府が提供する豊富な情報資源から、必要な文書を探し出すことが可能です。このサービスは、[Govbot](https://govbot.go.jp/)というサービスのために作られた政府情報の[データベース](https://www.soumu.go.jp/main_sosiki/hyouka/soudan_n/kyotsucb_top.html#faq)を利用しており、信頼性の高い情報へアクセスできます。また、AIを活用した要約機能を実験的に導入しており、検索結果の中から重要な情報を瞬時に把握することができます。ただし、要約は間違っていることもあるから気をつけてね。
+Govsearchは、国や自治体の制度を調べることができる検索アプリケーションです。[Govbot](https://govbot.go.jp/)というサービスのために政府が作成した[FAQ](https://www.soumu.go.jp/main_sosiki/hyouka/soudan_n/kyotsucb_top.html#faq)のデータを利用しており、質問文を入力するとFAQの中からそれに近いと思われる順に問いと回答を示します。また、検索結果を踏まえた要約と回答の生成も同時に行われます。要約と回答は試験的な機能であり、使用時には正確でない情報が含まれる場合があることにご注意ください。
 
-Govsearchは、Govbotと同様に政府文書のアクセシビリティ向上を目指して開発されましたが、インタラクティブなチャットボット形式ではなく、直接的な検索機能を中心に据えています。このプロジェクトは、AGPL3ライセンスの下で公開されており、その[コード](https://huggingface.co/spaces/hicustomer/govsearch/tree/main)はオンラインで自由に閲覧することができます。
+Govsearchは、Govbotと同様に政府文書のアクセシビリティ向上を目指して開発されましたが、インタラクティブなチャットボット形式ではなく、直接的な検索機能を中心に据えています。このプロジェクトは、AGPL3ライセンスの下で公開されており、その[ソースコード](https://github.com/hicustomer/govsearch)はオンラインで自由に閲覧することができます。
 `
 
 export function QAHelp() {
